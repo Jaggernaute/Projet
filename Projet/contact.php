01c680b2ab49b8cb8e0cc1b82885948db4abd26e
Jaggernaute <42418858+Jaggernaute@users.noreply.github.com>
2020-11-29 16:34:16 +0100

    29/11/2020
    j'ai rajouté :
    -une nav bar
    -un asside random entre 1 et 2

--- Projet/contact.php
+++ Projet/contact.php
@@ -1,35 +1,38 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact</title>
-    <link rel="stylesheet" href="css/style.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Contact</title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    <header>
-<h1>  Ca envois du bois  </h1>
-</header>
-<div class="coter">
-	<nav>
-		nav
-	</nav>
-	
-	<main>
-		accueil
-	</main>
+	<header>
+		<h1> Ca envois du bois </h1>
+	</header>
+	<div class="coter">
+		<nav>
+			<?php include('php/include/nav.php'); ?>
+		</nav>
 
-	<aside>
-		Promo -50% sur les nouveaux clients
-	</aside>
-</div>
+		<main>
+			accueil
+		</main>
 
-<footer>
-	<ul>
-    <li> -adresse: </li>
-	<li> -Mail: </li>
-	<li> -tel: </li>
-</footer>
+		<aside>
+			Promo -50% sur les nouveaux clients
+		</aside>
+	</div>
+
+	<footer>
+		<ul>
+			<li> -adresse: </li>
+			<li> -Mail: </li>
+			<li> -tel: </li>
+	</footer>
 
 </body>
+
 </html>
--- Projet/index.php
+++ Projet/index.php
@@ -1,34 +1,37 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
-    <link rel="stylesheet" href="css/style.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Accueil</title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-<header>
-<h1>  Ca envois du bois  </h1>
-</header>
-<div class="coter">
-	<nav>
-		nav
-	</nav>
-	
-	<main>
-		accueil
-	</main>
+	<header>
+		<?php include('php/include/header.php'); ?>
+	</header>
+	<div class="coter">
+		<nav>
+			<?php include('php/include/nav.php'); ?>
+		</nav>
 
-	<aside>
-		Promo -50% sur les nouveaux clients
-	</aside>
-</div>
+		<main>
+			accueil
+		</main>
 
-<footer>
-	<ul>
-    <li> -adresse: </li>
-	<li> -Mail: </li>
-	<li> -tel: </li>
-</footer>
+		<aside>
+			<?php include('php/include/asside/asside.php'); ?>
+		</aside>
+	</div>
+
+	<footer>
+		<ul>
+			<li> -adresse: </li>
+			<li> -Mail: </li>
+			<li> -tel: </li>
+	</footer>
 </body>
+
 </html>
